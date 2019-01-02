829e837ec0ae45cc09ba3f98ff6bf5073bed3eff
UTpH <UTpH@users.noreply.github.com>
2019-01-02 00:48:17 -0500

    Update
    Colab working code and final report

--- oneShotSiamese.py
+++ oneShotSiamese.py
@@ -35,8 +35,8 @@ def show_plot(iteration,loss):
     plt.show()
     
 class Config():
-    training_dir = "./data/faces/training/"    #change
-    testing_dir = "./data/faces/testing/"
+    training_dir = "G:/NYU_MS/Sem1/CV/Project/signatures/training/"    #change
+    testing_dir = "G:/NYU_MS/Sem1/CV/Project/signatures/testing/"
     train_batch_size = 64
     train_number_epochs = 100
     
@@ -49,6 +49,7 @@ class SiameseNetworkDataset(Dataset):
         
     def __getitem__(self,index):
         img0_tuple = random.choice(self.imageFolderDataset.imgs)
+        print("HEY : ", img0_tuple)
         #we need to make sure approx 50% of images are in the same class
         should_get_same_class = random.randint(0,1) 
         if should_get_same_class:
@@ -83,25 +84,6 @@ class SiameseNetworkDataset(Dataset):
     def __len__(self):
         return len(self.imageFolderDataset.imgs)
     
-folder_dataset = dset.ImageFolder(root=Config.training_dir)
-
-siamese_dataset = SiameseNetworkDataset(imageFolderDataset=folder_dataset,
-                                        transform=transforms.Compose([transforms.Resize((100,100)),
-                                                                      transforms.ToTensor()
-                                                                      ])
-                                       ,should_invert=False)
-    
-vis_dataloader = DataLoader(siamese_dataset,
-                        shuffle=True,
-                        num_workers=8,
-                        batch_size=8)
-dataiter = iter(vis_dataloader)
-
-
-example_batch = next(dataiter)
-concatenated = torch.cat((example_batch[0],example_batch[1]),0)
-imshow(torchvision.utils.make_grid(concatenated))
-print(example_batch[2].numpy())
 
 class SiameseNetwork(nn.Module):
     def __init__(self):
@@ -145,6 +127,7 @@ class SiameseNetwork(nn.Module):
         output1 = self.forward_once(input1)
         output2 = self.forward_once(input2)
         return output1, output2
+    
 class ContrastiveLoss(torch.nn.Module):
     """
     Contrastive loss function.
@@ -162,43 +145,60 @@ class ContrastiveLoss(torch.nn.Module):
 
 
         return loss_contrastive
+
+if __name__ == '__main__':
     
-train_dataloader = DataLoader(siamese_dataset,
-                        shuffle=True,
-                        num_workers=8,
-                        batch_size=Config.train_batch_size)
-
-net = SiameseNetwork()
-criterion = ContrastiveLoss()
-optimizer = optim.Adam(net.parameters(),lr = 0.0005 )
-
-device = 'cpu'
-if torch.cuda.is_available():
-    device = 'cuda'
-    print("Using CUDA")
-else:
-    print("Using CPU")
-if torch.cuda.device_count() > 1:
-  print("Using", torch.cuda.device_count(), "GPUs")
-  net = nn.DataParallel(net)
-net.to(device)
-
-counter = []
-loss_history = [] 
-iteration_number= 0
-
-for epoch in range(0,Config.train_number_epochs):
-    for i, data in enumerate(train_dataloader,0):
-        img0, img1 , label = data
-        img0, img1 , label = img0.to(device), img1.to(device) , label.to(device)
-        optimizer.zero_grad()
-        output1,output2 = net(img0,img1)
-        loss_contrastive = criterion(output1,output2,label)
-        loss_contrastive.backward()
-        optimizer.step()
-        if i %10 == 0 :
-            print("Epoch number {}\n Current loss {}\n".format(epoch,loss_contrastive.item()))
-            iteration_number +=10
-            counter.append(iteration_number)
-            loss_history.append(loss_contrastive.item())
-show_plot(counter,loss_history)
+    folder_dataset = dset.ImageFolder(root=Config.training_dir)
+    
+    siamese_dataset = SiameseNetworkDataset(imageFolderDataset=folder_dataset, transform=transforms.Compose([transforms.Resize((200,200)), transforms.ToTensor()]), should_invert=False)
+        
+    vis_dataloader = DataLoader(siamese_dataset, shuffle=True, num_workers=8, batch_size=8)
+    dataiter = iter(vis_dataloader)
+    
+    
+    example_batch = next(dataiter)
+    concatenated = torch.cat((example_batch[0],example_batch[1]),0)
+    imshow(torchvision.utils.make_grid(concatenated))
+    print(example_batch[2].numpy())
+        
+    train_dataloader = DataLoader(siamese_dataset,
+                            shuffle=True,
+                            num_workers=8,
+                            batch_size=Config.train_batch_size)
+    
+    net = SiameseNetwork()
+    criterion = ContrastiveLoss()
+    optimizer = optim.Adam(net.parameters(),lr = 0.0005 )
+    
+    device = 'cpu'
+# =============================================================================
+#     if torch.cuda.is_available():
+#         device = 'cuda'
+#         print("Using CUDA")
+#     else:
+#         print("Using CPU")
+#     if torch.cuda.device_count() > 1:
+#       print("Using", torch.cuda.device_count(), "GPUs")
+#       net = nn.DataParallel(net)
+#     net.to(device)
+# =============================================================================
+    
+    counter = []
+    loss_history = [] 
+    iteration_number= 0
+    
+    for epoch in range(0,Config.train_number_epochs):
+        for i, data in enumerate(train_dataloader,0):
+            img0, img1 , label = data
+            img0, img1 , label = img0.to(device), img1.to(device) , label.to(device)
+            optimizer.zero_grad()
+            output1,output2 = net(img0,img1)
+            loss_contrastive = criterion(output1,output2,label)
+            loss_contrastive.backward()
+            optimizer.step()
+            if i %10 == 0 :
+                print("Epoch number {}\n Current loss {}\n".format(epoch,loss_contrastive.item()))
+                iteration_number +=10
+                counter.append(iteration_number)
+                loss_history.append(loss_contrastive.item())
+    show_plot(counter,loss_history)
